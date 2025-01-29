a6becfac64453e09e7605eac53a5564c226f745e
jan1na <antilope666@gamil.com>
2025-01-29 18:24:22 +0100

    Fixed the models to enable pretrained=True without loosing pretrained weights. Added augmentations to dataset_janina.py

---
+++ scripts/models_janina.py
@@ -0,0 +1,58 @@
+import torch.nn as nn
+from torchvision import models
+
+USE_PRETRAINED = True  # Change to False if you want to train from scratch
+
+class MobileNetV2(nn.Module):
+    def __init__(self):
+        super(MobileNetV2, self).__init__()
+        self.model = models.mobilenet_v2(pretrained=USE_PRETRAINED)
+        self.model.classifier[1] = nn.Linear(self.model.last_channel, 1)  # Modify output layer
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        return self.sigmoid(self.model(x))
+
+class GoogleNet(nn.Module):
+    def __init__(self, aux_logits=False):
+        super(GoogleNet, self).__init__()
+        self.model = models.googlenet(pretrained=USE_PRETRAINED, aux_logits=aux_logits)
+        self.model.fc = nn.Linear(self.model.fc.in_features, 1)  # Modify output layer
+        self.sigmoid = nn.Sigmoid()
+        self.aux_logits = aux_logits
+
+    def forward(self, x):
+        x = self.model(x)
+        if self.aux_logits:  # Handle auxiliary outputs if enabled
+            x = x[0]  # Use main classifier output
+        return self.sigmoid(x)
+
+class ResNet(nn.Module):
+    def __init__(self):
+        super(ResNet, self).__init__()
+        self.model = models.resnet50(pretrained=USE_PRETRAINED)
+        self.model.fc = nn.Linear(self.model.fc.in_features, 1)  # Modify output layer
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        return self.sigmoid(self.model(x))
+
+class AlexNet(nn.Module):
+    def __init__(self):
+        super(AlexNet, self).__init__()
+        self.model = models.alexnet(pretrained=USE_PRETRAINED)
+        self.model.classifier[6] = nn.Linear(self.model.classifier[6].in_features, 1)  # Modify output layer
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        return self.sigmoid(self.model(x))
+
+class VGG19(nn.Module):
+    def __init__(self):
+        super(VGG19, self).__init__()
+        self.model = models.vgg19_bn(pretrained=USE_PRETRAINED)
+        self.model.classifier[6] = nn.Linear(self.model.classifier[6].in_features, 1)  # Modify output layer
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        return self.sigmoid(self.model(x))
